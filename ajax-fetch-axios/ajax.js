f7f3b9fc040fc908d0ecd94be5621592cc461f74
huangjianeng <1441820992@qq.com>
2021-01-13 19:54:52 +0800

    git 指令

--- ajax-fetch-axios/ajax.js
+++ ajax-fetch-axios/ajax.js
@@ -1,12 +1,2 @@
-var xhr = new XMLHttpRequest();
-console.log(xhr.readyState)
-xhr.onreadystatechange = function(){
-    console.log('------')
-    console.log('readyState:' + xhr.readyState)
-    console.log('ResponseHeaders:' + xhr.getAllResponseHeaders())
-    console.log('ResponseText:' + xhr.responseText.length)
-    console.log('------')
-}
-xhr.open('get','/')
-xhr.send(null)
+
 
